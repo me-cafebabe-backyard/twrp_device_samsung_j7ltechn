5e432dd1ddedcae97d88bf82b219ded7dd7c7ea6
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-15 22:24:03 +0800

    j7ltechn: Add omni product makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     twrp_j7ltechn-user \
     twrp_j7ltechn-userdebug \
     twrp_j7ltechn-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/omni_j7ltechn.mk
+
+COMMON_LUNCH_CHOICES += \
+    omni_j7ltechn-user \
+    omni_j7ltechn-userdebug \
+    omni_j7ltechn-eng
